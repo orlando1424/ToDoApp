0c16b23c94e66589ecd2ef45ac18cda157790d7d
Orlando <53872517+orlando1424@users.noreply.github.com>
2021-08-22 12:02:39 -0600

    cache

--- ToDoApp/ToDoApp/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ ToDoApp/ToDoApp/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace ToDoApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 1 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 2 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 3 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 4 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 5 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 6 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 7 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 8 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 9 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using ToDoApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/_Imports.razor"
+#line 10 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\_Imports.razor"
 using ToDoApp.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using ToDoApp.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "/Users/orlandolopez/Documents/Repos/ToDoApp/ToDoApp/ToDoApp/ToDoApp/Shared/SurveyPrompt.razor"
+#line 12 "D:\Repos\ToDoApp\ToDoApp\ToDoApp\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
